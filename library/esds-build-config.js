88486f217928f17ed0de4f0882a4c76a0af5c288
Kevin Powell <kevin@eightshapes.com>
2018-07-10 08:41:32 -0500

    #64 WIP form components

--- library/esds-build-config.js
+++ library/esds-build-config.js
@@ -1,5 +1,14 @@
 'use strict';
 module.exports = {
+  svgoConfig: {
+      plugins: [
+          {
+              removeAttrs: {
+                  attrs: 'stroke'
+              }
+          }
+      ]
+  },
   copyTasks: [
     {
       name: 'fonts',
