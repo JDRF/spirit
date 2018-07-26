e2332fee198757b70305496f1ae18e4f6d760c99
Kevin Powell <kevin@eightshapes.com>
2018-07-26 15:03:42 -0500

    #111 update card JS transpilation to use webroot version of the concatenated JS file

--- library/gulpfile.js
+++ library/gulpfile.js
@@ -3,15 +3,10 @@ const babel = require('gulp-babel');
 const rename = require("gulp-rename");
 
 gulp.task('compile-script-to-es5', () =>
-  gulp.src('dist/spirit.js')
+  gulp.src('_site/latest/scripts/spirit.js')
     .pipe(babel({presets: ['env']}))
     .pipe(rename("spirit.es5.js"))
-    .pipe(gulp.dest('dist'))
-);
-
-gulp.task('copy-es5-script-to-webroot', () =>
-  gulp.src('dist/spirit.es5.js')
     .pipe(gulp.dest('_site/latest/scripts'))
 );
 
-gulp.task('esds-hook:post:scripts:build:spirit', gulp.series('compile-script-to-es5', 'copy-es5-script-to-webroot'));
+gulp.task('esds-hook:post:scripts:build:spirit', gulp.series('compile-script-to-es5'));
