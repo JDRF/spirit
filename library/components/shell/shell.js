0525619684b4c19ed6cc641dac13c82650054ca6
Joe Snell <joesnellpdx@gmail.com>
2018-09-05 10:58:37 -0700

    feature/127--page-shell: $127 - setup page shell macros for shell, header, main, and footer - update sink page - update styles - add script to give html a class spirit-shell

---
+++ library/components/shell/shell.js
@@ -0,0 +1,15 @@
+/**
+ * Spirit Shell Scripts
+ *
+ * @return {void}
+ */
+const spiritShell = () => {
+  const root = document.documentElement;
+
+  if (root) {
+    // Add html class for shell
+    root.classList.add('spirit-shell');
+  }
+};
+
+spiritShell();
