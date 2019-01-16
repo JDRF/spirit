90c6ba436768026964e629ab9c097060a564180a
Joe Snell <joesnellpdx@gmail.com>
2019-01-16 14:38:03 -0800

    feature/185--specialized-form-inputs: add mmyy options to date input

--- library/components/form/form.js
+++ library/components/form/form.js
@@ -1,8 +1,9 @@
 class SpiritFormInputMasks {
-    static enable() {
-      Inputmask({mask: "99/99/9999", placeholder: "MM/DD/YYYY"}).mask('.spirit-form__input--date-mask');
-      Inputmask({mask: "(999) 999-9999"}).mask('.spirit-form__input--tel-mask');
-    }
+  static enable() {
+    Inputmask({ mask: "99/99/9999", placeholder: "MM/DD/YYYY" }).mask('.spirit-form__input--mask-date');
+    Inputmask({ mask: "99/99", placeholder: "MM/YY" }).mask('.spirit-form__input--mask-date-mmyy');
+    Inputmask({ mask: "(999) 999-9999" }).mask('.spirit-form__input--mask-tel');
+  }
 }
 
 SpiritFormInputMasks.enable();
