dbd079b02c2d0dfa7dd1168866269d6b733dbba1
Joe Snell <joesnellpdx@gmail.com>
2019-01-17 11:06:07 -0800

    feature/185--specialized-form-inputs: change masks to lowercase

--- library/components/form/form.js
+++ library/components/form/form.js
@@ -1,8 +1,11 @@
+/* global Inputmask */
 class SpiritFormInputMasks {
   static enable() {
-    Inputmask({ mask: "99/99/9999", placeholder: "MM/DD/YYYY" }).mask('.spirit-form__input--mask-date');
-    Inputmask({ mask: "99/99", placeholder: "MM/YY" }).mask('.spirit-form__input--mask-date-mmyy');
+    /* eslint-disable new-cap */
+    Inputmask({ mask: "99/99/9999", placeholder: "mm/dd/yyyy" }).mask('.spirit-form__input--mask-date');
+    Inputmask({ mask: "99/99", placeholder: "mm/yy" }).mask('.spirit-form__input--mask-date-mmyy');
     Inputmask({ mask: "(999) 999-9999" }).mask('.spirit-form__input--mask-tel');
+    /* eslint-enable new-cap */
   }
 }
 
