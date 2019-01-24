0cbc3320d1a70b63a63305a434f4b857060ebe19
Joe Snell <joesnellpdx@gmail.com>
2019-01-24 08:44:55 -0800

    feature/185--specialized-form-inputs: add inputmask scripts to doc_site

--- doc-site/esds-build-config.js
+++ doc-site/esds-build-config.js
@@ -9,58 +9,63 @@ module.exports = {
         {
             name: 'script-dependencies',
             sources: ['node_modules/prismjs/prism.js', // required by esds-doc
-                        'node_modules/prismjs/components/prism-json.min.js', // required by esds-doc
-                        'node_modules/prismjs/components/prism-scss.min.js', // required by esds-doc
-                        'node_modules/prismjs/components/prism-twig.min.js', // required by esds-doc
-                        'node_modules/svg4everybody/dist/svg4everybody.min.js',
-                        'node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js',
-                        'node_modules/scrollmonitor/scrollMonitor.js',
-                        'node_modules/scrollmonitor/scrollMonitor.js.map',
-                        `node_modules/esds-doc/dist/esds-doc.js`],
+                'node_modules/prismjs/components/prism-json.min.js', // required by esds-doc
+                'node_modules/prismjs/components/prism-scss.min.js', // required by esds-doc
+                'node_modules/prismjs/components/prism-twig.min.js', // required by esds-doc
+                'node_modules/svg4everybody/dist/svg4everybody.min.js',
+                'node_modules/smoothscroll-polyfill/dist/smoothscroll.min.js',
+                'node_modules/scrollmonitor/scrollMonitor.js',
+                'node_modules/scrollmonitor/scrollMonitor.js.map',
+                `node_modules/esds-doc/dist/esds-doc.js`,
+                "node_modules/inputmask/dist/inputmask/dependencyLibs/inputmask.dependencyLib.js",
+                "node_modules/inputmask/dist/inputmask/inputmask.js",
+                "node_modules/inputmask/dist/inputmask/inputmask.extensions.js",
+                "node_modules/inputmask/dist/inputmask/inputmask.numeric.extensions.js",
+                "node_modules/inputmask/dist/inputmask/inputmask.date.extensions.js"],
             destination: `../docs/scripts/dependencies`,
             watch: true
         },
         {
             name: 'spirit-styles',
             sources: [
-                    'node_modules/@jdrfhq/spirit/dist/spirit.css',
-                    'node_modules/@jdrfhq/spirit/dist/spirit.min.css',
-                    'node_modules/prismjs/themes/prism.css'], // required by esds-doc
+                'node_modules/@jdrfhq/spirit/dist/spirit.css',
+                'node_modules/@jdrfhq/spirit/dist/spirit.min.css',
+                'node_modules/prismjs/themes/prism.css'], // required by esds-doc
             destination: '../docs/styles',
             watch: true
         },
         {
             name: 'spirit-scripts',
             sources: [
-                    'node_modules/@jdrfhq/spirit/dist/spirit.js'],
+                'node_modules/@jdrfhq/spirit/dist/spirit.js'],
             destination: '../docs/scripts/dependencies',
             watch: true
         },
         {
             name: 'spirit-icon',
             sources: [
-                    'node_modules/@jdrfhq/spirit/dist/spirit.svg'],
+                'node_modules/@jdrfhq/spirit/dist/spirit.svg'],
             destination: '../docs/icons',
             watch: true
         },
         {
             name: 'spirit-brushes',
             sources: [
-                    'node_modules/@jdrfhq/spirit/dist/spirit-brushes.svg'],
+                'node_modules/@jdrfhq/spirit/dist/spirit-brushes.svg'],
             destination: '../docs/brushes',
             watch: true
         },
         {
             name: 'spirit-fonts',
             sources: [
-                    'node_modules/@jdrfhq/spirit/fonts/**/*'],
+                'node_modules/@jdrfhq/spirit/fonts/**/*'],
             destination: '../docs/fonts',
             watch: true
         },
         {
             name: 'spirit-icon-data',
             sources: [
-                    'node_modules/@jdrfhq/spirit/data/icons.json'],
+                'node_modules/@jdrfhq/spirit/data/icons.json'],
             destination: 'data',
             rename: 'spirit_icons.json',
             watch: true
