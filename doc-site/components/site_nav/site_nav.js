e610f31a2ab03a9ada0e93a9d617c2b9057f5b88
Joe Snell <joesnellpdx@gmail.com>
2019-04-03 10:04:26 -0700

    feature/#379-flip-hamburger-nav: update keyboard command for navigation

--- doc-site/components/site_nav/site_nav.js
+++ doc-site/components/site_nav/site_nav.js
@@ -31,6 +31,11 @@ class SpiritDocSiteNavToggle {
 
     // Close navigation with esc or click outside navigation
     const listenerCloseOpenMenus = function(e) {
+      // Open and close nav with 'return'
+      if (e.type === 'keyup' && e.keyCode === 13) {
+        toggleNav();
+      }
+
       if (document.documentElement.classList.contains('-spirit-no-scroll')) {
 
         // if the event is keyup and it was the ESC key
@@ -80,6 +85,7 @@ class SpiritDocSiteNavToggle {
     };
 
     const toggleNav = function () {
+      console.log('toggled');
       const expanded = toggle.getAttribute('aria-expanded');
       const firstLink = navContain.querySelectorAll('.spirit-vertical-nav__link')[0];
 
