bf7bc8a6ee740855325f7601723f35ddf3694299
Joe Snell <joesnellpdx@gmail.com>
2020-02-20 15:25:56 -0800

    Update UX for password toggle
    - use icon button instead of <a>
    - update styles to container to match a spirit input
    - update scripts to toggle inputs / container
    - update sink page
    - add caller ability to button njk file
    - add ability to add aria-label to link

--- library/components/form/form.js
+++ library/components/form/form.js
@@ -16,24 +16,40 @@ class SpiritFormPasswordToggle {
     const passwordToggleInputs = Array.from(document.querySelectorAll('.spirit-form__input--password-toggle'));
     passwordToggleInputs.forEach(i => {
       const input = i.querySelector('input');
-      const trigger = i.querySelector('a');
+      const trigger = i.querySelector('.spirit-form__input-password-toggle');
+      const triggerIconHide = trigger.querySelector('svg[aria-label="Hide"]');
+      const triggerIconShow = trigger.querySelector('svg[aria-label="Show"]');
 
-      if (trigger.innerText === 'Show') {
+      // A11y Setup
+      if (triggerIconHide.getAttribute("aria-hidden") === 'true') {
         trigger.setAttribute('aria-pressed', 'false');
       } else {
         trigger.setAttribute('aria-pressed', 'true');
       }
 
+      // On input focus
+      input.addEventListener('focus', () => {
+        i.classList.toggle('spirit-form__input--password-toggle--focus');
+      });
+
+      // In input blur
+      input.addEventListener('blur', () => {
+        i.classList.toggle('spirit-form__input--password-toggle--focus');
+      });
+
+      // On toggle show password click
       trigger.addEventListener('click', e => {
         e.preventDefault();
         if (input.type === 'text') {
           input.type = 'password';
-          trigger.textContent = 'Show';
           trigger.setAttribute('aria-pressed', 'false');
+          triggerIconHide.setAttribute('aria-hidden', 'false');
+          triggerIconShow.setAttribute('aria-hidden', 'true');
         } else {
           input.type = 'text';
-          trigger.textContent = 'Hide';
           trigger.setAttribute('aria-pressed', 'true');
+          triggerIconHide.setAttribute('aria-hidden', 'true');
+          triggerIconShow.setAttribute('aria-hidden', 'false');
         }
       });
     });
