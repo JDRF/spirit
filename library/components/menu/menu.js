4eb301c453939508e5abd2cdbee1abd4bc8563b0
Ryan Sommers <ryan@10up.com>
2020-04-16 16:21:42 -0700

    Adds Menu component, initial styling
    Signed-off-by: Ryan Sommers <ryan@10up.com>

---
+++ library/components/menu/menu.js
@@ -0,0 +1,58 @@
+
+/*global A11yDialog*/
+
+const spiritMenus = function() {
+	if (typeof A11yDialog !== "function") {
+			return;
+	}
+
+	const menus = Array.from(document.querySelectorAll('.spirit-menu'));
+
+	if (!menus) {
+			return;
+	}
+
+	// Defaults
+	const defaults = {
+			container: '#spirit-shell-main',
+			noScrollClass: '-spirit-no-scroll'
+	};
+
+	menus.forEach(item => {
+			const menu = new A11yDialog(item, defaults.container);
+			const spiritMenu = item.querySelector('.spirit-menu__menu');
+
+			const maybeDisableEscToClose = (e) => {
+					if (spiritMenu && spiritMenu.getAttribute('role') === 'alertdialog' && e.which === 27) {
+							e.preventDefault();
+					}
+			};
+
+			menu.on('show', function (el) {
+
+					// Ensure focus is on first item - wait until animation finished first
+					spiritMenu.addEventListener('transitionend', transitionEndCallback);
+
+					function transitionEndCallback() {
+							const focusableChildren = spiritMenu.querySelectorAll('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+							const focused = spiritMenu.querySelector('[autofocus]') || focusableChildren[0];
+
+							if (focused) {
+									focused.focus();
+							}
+
+							spiritMenu.removeEventListener('transitionend', transitionEndCallback);
+					}
+
+					document.body.classList.add(defaults.noScrollClass);
+					document.addEventListener('keydown', maybeDisableEscToClose);
+			});
+
+			menu.on('hide', function (el) {
+					document.body.classList.remove(defaults.noScrollClass);
+					document.removeEventListener('keydown', maybeDisableEscToClose);
+			});
+	});
+};
+
+spiritMenus();
